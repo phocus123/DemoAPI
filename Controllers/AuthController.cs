2cc92cc04075b5ce510d30c14126513d0e910443
Mark Schafers <mark_schafers@hotmail.com>
2018-04-07 14:15:50 +0800

    Finished user authorization logic

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -10,8 +16,10 @@ namespace DatingApp.API.Controllers
   public class AuthController : Controller
   {
     private readonly IAuthRepository _repo;
-    public AuthController(IAuthRepository repo)
+    private readonly IConfiguration _config;
+    public AuthController(IAuthRepository repo, IConfiguration config)
     {
+      _config = config;
       _repo = repo;
     }
 
@@ -34,6 +42,34 @@ namespace DatingApp.API.Controllers
       var createUser = await _repo.Register(userToCreate, userForRegisteredDto.password);
 
       return StatusCode(201);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
+    {
+      var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+      if (userFromRepo == null)
+        return Unauthorized();
+
+      // Generate Token.
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.ASCII.GetBytes(_config.GetSection("AppSettings:Token").Value);
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+            new Claim(ClaimTypes.Name, userFromRepo.Username)
+        }),
+        Expires = DateTime.Now.AddHours(1),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+          SecurityAlgorithms.HmacSha512Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      var tokenString = tokenHandler.WriteToken(token);
+
+      return Ok(new { tokenString });
     }
   }
 }
