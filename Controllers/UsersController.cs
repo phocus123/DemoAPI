5003d5f1a818e6e797d745ea7dd753381578a1b2
Mark Schafers <mark_schafers@hotmail.com>
2018-04-14 10:12:06 +0800

    added functionality for editing profiles to API

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -8,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
-
+  [Authorize]
   [Route("api/[controller]")]
   public class UsersController : Controller
   {
@@ -38,6 +40,30 @@ namespace DatingApp.API.Controllers
       var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
       return Ok(userToReturn);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, [FromBody]UserForUpdateDto userForUpdateDto)
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+      var userFromRepo = await _repo.GetUser(id);
+
+      if (userFromRepo == null)
+        return NotFound($"Could not find user with an id of {id}");
+
+      if (currentUserId != userFromRepo.Id)
+        return Unauthorized();
+
+      _mapper.Map(userForUpdateDto, userFromRepo);
+
+      if (await _repo.SaveAll())
+        return NoContent();
+
+      throw new Exception($"Updating user {id} failed on save");
     }
   }
 }
